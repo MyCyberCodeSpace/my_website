92dc6536389704696a63006cd0cf5ddeedc8c02d
Your Name <gabdeveloper@protonmail.com>
2025-07-05 16:41:53 -0300

    upload

---
+++ teste.py
@@ -0,0 +1,21 @@
+
+
+
+notas = [
+    10, 10, 9.5,
+    8.9, 9.9, 9.4,
+    10, 9.9, 9.4,
+    9.45, 9.4, 10,
+    8.7, 9.4, 10,
+    10, 9.4, 
+    9.5, 8.9, 8.3,
+    9.8, 8.4, 10, 
+    10, 8.9, 9.8,
+    10, 10, 10,
+]
+
+media = (sum(notas) / len(notas))
+print(media)
+
+string = 'MOBILE DEVELOPMENT'
+print(string.lower())
